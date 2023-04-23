b5467108df29eb7e8229d63709b2f11fee6cd42e
Fernando Jorge <fernandojorge.cavalcantegomes@gmail.com>
2023-04-23 19:03:57 -0300

    fix wall bug

--- constants.py
+++ constants.py
@@ -1,3 +1,6 @@
+import pygame
+pygame.init()
+
 # Colors
 BLACK = (0, 0, 0)
 RED = (255, 255, 255)
@@ -6,7 +9,7 @@ GREEN = (0, 255, 0)
 
 # Screen
 ORIGINAL_TILE_SIZE = 16
-SCALE = 2
+SCALE = 3
 TILE_SIZE = ORIGINAL_TILE_SIZE * SCALE
 
 SCREEN_COLUMNS = 24
@@ -14,3 +17,8 @@ SCREEN_ROWS = 16
 
 SCREEN_WIDTH = SCREEN_COLUMNS * TILE_SIZE
 SCREEN_HEIGHT = SCREEN_ROWS * TILE_SIZE
+
+# Fonts
+GAP_WORD = 21
+INCONSOLATA_SMALL_FONT = pygame.font.Font(
+    "fonts/Inconsolata-VariableFont_wdth,wght.ttf", 21)
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from constants import *
 
 from entities.Player import Player
 from entities.Tile import Tile
+from objects.Page import Page
+
 from KeyHandler import KeyHandler
 import pygame
 pygame.init()
@@ -15,11 +17,13 @@ player = Player()
 floor = Tile('./sprites/tiles/floor.png')
 wall = Tile('./sprites/tiles/wall.png')
 book = Tile('./sprites/tiles/open-book.png')
-page = Tile('./sprites/tiles/page.png')
+page_tile = Tile('./sprites/tiles/page.png')
 bed = Tile('./sprites/tiles/cama.png')
 mesa = Tile('./sprites/tiles/mesa.png')
 keyH = KeyHandler()
 readBook = False
+
+page = Page(page_tile)
 
 pygame.display.set_caption("Aurelius")
 exit = False
@@ -36,14 +40,19 @@ while not exit:
     # Draw floor
     for col in range(SCREEN_COLUMNS):
         for row in range(SCREEN_ROWS):
-            wall.draw(screen, 0, row)
-            wall.draw(screen, 23, col)
-            wall.draw(screen, col, 0) 
             floor.draw(screen, col, row)
-    
+
+    for row in range(SCREEN_ROWS):
+        wall.draw(screen, 0, row)
+        wall.draw(screen, 23, row)
+
+    for col in range(SCREEN_COLUMNS):
+        wall.draw(screen, col, 0)
+        wall.draw(screen, col, 15)
+
     mesa.draw(screen, 12, 8)
     book.draw(screen, 12, 8)
-    bed.draw(screen,12, 1)
+    bed.draw(screen, 12, 1)
 
     player.update()
     player.draw(screen)
@@ -55,8 +64,6 @@ while not exit:
         readBook = False
 
     if readBook:
-        for col in range(3, 21):
-            for row in range(3, 13):
-                page.draw(screen, col, row)
+        page.draw(screen)
 
     pygame.display.update()
